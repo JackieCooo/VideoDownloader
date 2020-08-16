46f0332a3360ea1b81aee0709fffcc19dc5940c2
JackieCoo <1131390120@qq.com>
2020-08-16 14:22:56 +0800

    Working on Alpha 1.0

---
+++ Main.py
@@ -0,0 +1,200 @@
+import sys
+import os
+from PyQt5 import QtWidgets, QtGui, QtCore
+from VideoSource import bilibili
+
+
+class VideoDownloader(object):
+
+    def __init__(self):
+        self.engine_list = ['b站', '...']
+        self.sess = None
+
+    def setup_ui(self):
+        self.main_window = QtWidgets.QMainWindow()
+        self.main_window.setObjectName("main_window")
+        self.main_window.resize(1200, 800)
+        # self.main_window.setWindowIcon()
+        self.main_window.setWindowTitle("Video Downloader - Designed by Jackie")
+        self.central_widget = QtWidgets.QWidget(self.main_window)
+        self.central_widget.setObjectName("central_widget")
+
+        # 设置侧边栏
+        self.left_widget = QtWidgets.QWidget(self.central_widget)
+        self.left_widget.setGeometry(QtCore.QRect(0, 0, 250, 800))
+        self.left_widget.setObjectName("left_widget")
+        self.v_box_1 = QtWidgets.QVBoxLayout(self.left_widget)
+        self.btn1 = QtWidgets.QPushButton("搜索", self.left_widget)
+        self.btn2 = QtWidgets.QPushButton("设置", self.left_widget)
+        self.logo = QtWidgets.QLabel(self.left_widget)
+        self.logo.setObjectName("logo")
+        self.spacer1 = QtWidgets.QSpacerItem(250, 100, QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Preferred)
+        self.spacer2 = QtWidgets.QSpacerItem(250, 500, QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Preferred)
+        self.v_box_1.addWidget(self.logo)
+        self.v_box_1.addItem(self.spacer1)
+        self.v_box_1.addWidget(self.btn1)
+        self.v_box_1.addWidget(self.btn2)
+        self.v_box_1.addItem(self.spacer2)
+
+        # 设置右侧区域
+        self.right_widget = QtWidgets.QStackedWidget(self.central_widget)
+        self.right_widget.setObjectName("right_widget")
+        self.right_widget.setGeometry(QtCore.QRect(250, 0, 1200, 800))
+
+        # 下载页设置
+        self.download_page = QtWidgets.QWidget()
+        self.download_page.setObjectName("download_page")
+        self.download_page.setGeometry(QtCore.QRect(250, 0, 1200, 800))
+        self.search_area = QtWidgets.QWidget(self.download_page)
+        self.search_area.setGeometry(QtCore.QRect(0, 0, 950, 60))
+        self.search_area.setObjectName("search_area")
+        self.h_box_1 = QtWidgets.QHBoxLayout(self.search_area)
+
+        # 搜索框设置
+        self.search_box = QtWidgets.QLineEdit(self.search_area)
+        sp = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
+        sp.setHorizontalStretch(6)
+        self.search_box.setSizePolicy(sp)
+        self.search_box.setFixedHeight(40)
+        self.search_box.setObjectName("search_box")
+        font = QtGui.QFont()
+        font.setFamily("微软雅黑")
+        font.setPointSize(12)
+        self.search_box.setTextMargins(5, 0, 0, 0)
+        self.search_box.setFont(font)
+        self.search_box.placeholderText()
+        self.search_box.setPlaceholderText("输入视频地址")
+        self.search_box.setCursor(QtCore.Qt.IBeamCursor)
+
+        # 按钮设置
+        self.search_btn = QtWidgets.QPushButton("添加", self.search_area)
+        sp = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
+        sp.setHorizontalStretch(1)
+        self.search_btn.setSizePolicy(sp)
+        self.search_btn.setFixedHeight(40)
+        font = QtGui.QFont()
+        font.setFamily("微软雅黑")
+        font.setPointSize(12)
+        self.search_btn.setFont(font)
+        self.search_btn.setObjectName("search_btn")
+        self.search_btn.setCursor(QtCore.Qt.PointingHandCursor)
+        self.search_btn.clicked.connect(lambda: self.search(self.search_box.text()))  # 按钮触发信号
+
+        # 引擎切换设置
+        self.engine = QtWidgets.QComboBox(self.search_area)
+        self.engine.setSizePolicy(sp)
+        self.engine.setFixedHeight(40)
+        self.engine.setObjectName("engine")
+        self.engine.setFrame(True)
+        combobox_text = QtWidgets.QLineEdit()  # 设置combobox字体
+        combobox_text.setReadOnly(True)
+        combobox_text.setAlignment(QtCore.Qt.AlignCenter)
+        palette = QtGui.QPalette()
+        brush = QtGui.QBrush(QtGui.QColor(0, 0, 0))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.Text, brush)
+        palette.setBrush(QtGui.QPalette.Inactive, QtGui.QPalette.Text, brush)
+        palette.setBrush(QtGui.QPalette.Disabled, QtGui.QPalette.Text, brush)
+        self.engine.setPalette(palette)
+        self.engine.setLineEdit(combobox_text)
+        combobox_drop_down = QtWidgets.QListWidget()  # 设置combobox下拉菜单字体
+        for i in range(2):
+            item = QtWidgets.QListWidgetItem(self.engine_list[i])
+            item.setTextAlignment(QtCore.Qt.AlignCenter)
+            combobox_drop_down.addItem(item)
+        self.engine.setModel(combobox_drop_down.model())
+        self.engine.setView(combobox_drop_down)
+        font = QtGui.QFont()
+        font.setFamily("微软雅黑")
+        font.setBold(True)
+        font.setPointSize(10)
+        self.engine.setFont(font)
+        self.engine.setCursor(QtCore.Qt.PointingHandCursor)
+        self.engine.currentIndexChanged.connect(self.engine_switch)  # 搜索引擎切换
+        
+        self.h_box_1.setContentsMargins(10, 10, 10, 10)
+        self.h_box_1.setSpacing(0)
+        self.h_box_1.addWidget(self.engine)
+        self.h_box_1.addWidget(self.search_box)
+        self.h_box_1.addWidget(self.search_btn)
+
+        # 基本信息设置
+        self.info_box = QtWidgets.QTabWidget(self.download_page)
+        self.info_box.setObjectName("info_box")
+        self.info_box.setGeometry(QtCore.QRect(0, 90, 950, 760))
+
+        # 队列
+        self.sequence = QtWidgets.QWidget()
+        self.table = QtWidgets.QTableWidget(self.sequence)
+        self.table.setGeometry(QtCore.QRect(0, 0, 950, 700))
+        font = QtGui.QFont()
+        font.setFamily("微软雅黑")
+        font.setPointSize(10)
+        self.table.setFont(font)
+        self.table.setColumnCount(6)
+        # self.table.setRowCount(20)
+        self.table.setHorizontalHeaderLabels(['', '视频名', '时长', '大小', '画质', ''])
+        self.table.setMouseTracking(True)
+        self.table.setSelectionMode(QtWidgets.QAbstractItemView.NoSelection)
+        self.table.setShowGrid(False)
+        self.table.setGridStyle(QtCore.Qt.NoPen)
+        self.table.horizontalHeader().setStretchLastSection(True)
+        self.table.verticalHeader().setVisible(False)
+        self.table.horizontalHeader().resizeSection(0, 220)
+        self.table.horizontalHeader().resizeSection(1, 350)
+        self.table.horizontalHeader().resizeSection(2, 100)
+        self.table.horizontalHeader().resizeSection(3, 100)
+        self.table.horizontalHeader().resizeSection(4, 100)
+        self.table.setFrameShape(QtWidgets.QFrame.NoFrame)
+        self.table.setObjectName("table")
+
+        # 正在下载
+        self.downloading = QtWidgets.QWidget()
+
+        # 已完成
+        self.finished = QtWidgets.QWidget()
+
+        self.info_box.addTab(self.sequence, "队列")
+        self.info_box.addTab(self.downloading, "正在下载")
+        self.info_box.addTab(self.finished, "已完成")
+
+        # 设置设置页
+        self.option_page = QtWidgets.QWidget()
+        self.option_page.setObjectName("option_page")
+        self.option_page.setGeometry(QtCore.QRect(250, 0, 1200, 800))
+
+        self.right_widget.addWidget(self.download_page)
+        self.right_widget.addWidget(self.option_page)
+        self.right_widget.setCurrentIndex(0)
+
+        self.main_window.setCentralWidget(self.central_widget)
+        self.main_window.show()
+
+    def engine_switch(self):
+        print(f'当前搜索引擎：{self.engine.currentIndex()}')
+        if self.engine.currentIndex() == 0:
+            self.sess = bilibili.bilibiliVideo()
+        elif self.engine.currentIndex() == 1:
+            pass
+        elif self.engine.currentIndex() == 2:
+            pass
+        elif self.engine.currentIndex() == 3:
+            pass
+        elif self.engine.currentIndex() == 4:
+            pass
+        elif self.engine.currentIndex() == 5:
+            pass
+
+    def search(self, text):
+        pass
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    run = VideoDownloader()
+    run.setup_ui()
+    run.engine_switch()
+    with open('StyleSheet.qss', 'r') as f:
+        style = f.read()
+    app.setStyleSheet(style)
+    sys.exit(app.exec_())
---
+++ VideoSource/bilibili.py
@@ -0,0 +1,129 @@
+import requests
+import re
+import os
+
+
+class bilibiliVideo(object):
+
+    def __init__(self):
+        self.header1 = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0) Gecko/20100101 Firefox/6.0'
+        }
+        self.header2 = {
+            'Connection': 'keep-alive',
+            'Pragma': 'no-cache',
+            'Cache-Control': 'no-cache',
+            'Origin': 'https://www.bilibili.com',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36',
+            'Accept': '*/*',
+            'Sec-Fetch-Site': 'cross-site',
+            'Sec-Fetch-Mode': 'cors',
+            # 'Referer': f'{self.url}',  # 视频地址
+            'Accept-Encoding': 'identity',
+            'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+            # 'Range': f'bytes=0-{self.size}',  # 视频长度
+        }
+        self.url = None
+        self.video_url = []  # 112:1080p+ 80:1080p 64:720p 32:480p 16:360p
+        self.audio_url = []  # 30280: 160kbps 30216: 80kbps
+        self.filename = None
+        self.size = None
+        # self.vq = 0  # 视频质量: 0:最高画质优先 1:1080p+ 2:1080p 3:720p 4:480p 5:360p
+        # self.aq = 0  # 音频质量: 0:最高音质优先 1:160kbps 2:80kbps
+        self.duration = None
+
+    def get_video(self, url):
+        self.header2.update({'Range': f'bytes=0-{self.size}'})
+        res = requests.get(url=url, headers=self.header2)
+        with open('../temp/video.flv', 'wb') as f:
+            for data in res.iter_content(1024):
+                f.write(data)
+
+    def get_audio(self, url):
+        self.header2.update({'Range': f'bytes=0-{self.size}'})
+        res = requests.get(url=url, headers=self.header2)
+        with open('../temp/audio.mp3', 'wb') as f:
+            for data in res.iter_content(1024):
+                f.write(data)
+
+    def merge_file(self):
+        video_path = "../temp/video.flv"
+        audio_path = "../temp/audio.mp3"
+        os.system("ffmpeg -i " + video_path + " -i " + audio_path + " -codec copy ../downloads/final.mp4")
+        os.rename("../downloads/final.mp4", f"../downloads/{self.filename}.mp4")
+        os.remove("../temp/video.flv")
+        os.remove("../temp/audio.mp3")
+
+    def get_info(self, url):
+        self.url = url  # 视频地址
+        self.header2.update({'Referer': f'{self.url}'})  # 插入Referer
+        response = requests.get(url=self.url, headers=self.header1).text  # 类型为str
+        # print(response)
+        res = eval(re.search(r'(?<=window.__playinfo__=).*?(?=</script><script>)', response).group())["data"]["dash"]  # 类型为dict
+        # print(res)
+        for i in res["video"]:
+            temp = [i["id"], i["baseUrl"], i["bandwidth"]]
+            self.video_url.append(temp)
+        # print(self.video_url)
+        for i in res["audio"]:
+            temp = [i["id"], i["baseUrl"], i["bandwidth"]]
+            self.audio_url.append(temp)
+        # print(self.audio_url)
+        self.filename = re.search(r'(?<="title":").*?(?=",)', response).group(0)
+        # print(self.filename)
+        self.duration = int(re.search(r'(?<="timelength":).*?(?=,)', response).group())
+        pic_url = re.search(r'(?<=itemprop="image" content=").*?(?=">)', response).group(0)
+        pic = requests.get(url=pic_url, headers=self.header1).content  # 获取封面图
+        with open('../temp/pic.jpg', 'wb') as f:
+            f.write(pic)
+        return self.filename, self.duration
+
+    def download(self, vq, aq):
+        print("下载视频")
+        if vq == 0:
+            self.size = int((self.duration / 1000) * self.video_url[0][2] / 8)
+            self.get_video(self.video_url[0][1])
+        else:
+            for i in self.video_url:
+                if i[0] == 112 and vq == 1:
+                    self.size = int((self.duration / 1000) * i[2] / 8)
+                    self.get_video(i[1])
+                    break
+                elif i[0] == 80 and vq == 2:
+                    self.size = int((self.duration / 1000) * i[2] / 8)
+                    self.get_video(i[1])
+                    break
+                elif i[0] == 64 and vq == 3:
+                    self.size = int((self.duration / 1000) * i[2] / 8)
+                    self.get_video(i[1])
+                    break
+                elif i[0] == 32 and vq == 4:
+                    self.size = int((self.duration / 1000) * i[2] / 8)
+                    self.get_video(i[1])
+                    break
+                elif i[0] == 16 and vq == 5:
+                    self.size = int((self.duration / 1000) * i[2] / 8)
+                    self.get_video(i[1])
+                    break
+                else:
+                    print("无此分辨率视频")
+        print("下载音频")
+        if aq == 0:
+            self.size = int((self.duration / 1000) * self.audio_url[0][2] / 8)
+            self.get_audio(self.audio_url[0][1])
+        else:
+            for i in self.audio_url:
+                if i[0] == 112 and aq == 1:
+                    self.size = int((self.duration / 1000) * i[2] / 8)
+                    self.get_video(i[1])
+                    break
+                elif i[0] == 80 and aq == 2:
+                    self.size = int((self.duration / 1000) * i[2] / 8)
+                    self.get_video(i[1])
+                    break
+        self.merge_file()
+
+
+if __name__ == '__main__':
+    app = bilibiliVideo()
+    # app.run()
